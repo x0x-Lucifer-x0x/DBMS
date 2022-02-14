ee7672e0e647e7a2d1f16866fabb4319f5cce60e
TheCrazyDICK <skaromal201@gmail.com>
2022-02-14 15:38:05 +0530

    Restore to working

--- Login_page.py
+++ Login_page.py
@@ -1,8 +1,6 @@
 from tkinter import * 
 from PIL import Image,ImageTk
-
-
-
+from forgot_pwd import *
 
 class Login:
     def __init__(self, window):
@@ -10,47 +8,44 @@ class Login:
         self.window.title("LOGIN PAGE")
         self.window.geometry("1920x1080")
         self.window.config(bg="#f6f6f6")
-    
+
 
         #Bg img
-        image = Image.open('hbg.jpg')
-        image =  image.resize((1600,  850), Image.ANTIALIAS)
-        my_img = ImageTk.PhotoImage( image)
-        my_lbl = Label(image =  my_img) 
-        my_lbl.pack()
+        self.image = Image.open('hbg.jpg')
+        self.image = self.image.resize((1250,  800), Image.ANTIALIAS)
+        self.my_img = ImageTk.PhotoImage(self.image)
+        self.my_lbl = Label(image = self.my_img) 
+        self.my_lbl.pack()
 
         #Login label
-        self.l1 = Label(text="Username",bg="#f6f6f6", font=('Arial', 13))
+        self.l1 = Label(text="Username",bg="#f6f6f6", font=('Arial', 11))
         self.l1.place(x=30,y=270,height=30,width=100)
         #Login entry
         self.e1 = Entry(text="",bg='#EFEFEF',relief="flat")
         self.e1.place(x=45,y=300,height=50,width=255)
 
         #Password label
-        self.l2 = Label(text="Password",bg="#f6f6f6", font=('Arial', 13))
+        self.l2 = Label(text="Password",bg="#f6f6f6", font=('Arial', 11))
         self.l2.place(x=30,y=390,height=30,width=100)
         #Password entry
-        e2 = Entry(bg="#EFEFEF",relief="flat")
-        e2.place(x=45,y=420,height=50,width=255)
+        self.e2 = Entry(bg="#EFEFEF",relief="flat")
+        self.e2.place(x=45,y=420,height=50,width=255)
 
         #Forgot_pwd Button
-        self.f_pwd = Button(text="forgot password?",command=f_pwd,bd=0, fg="Black",underline=0 ,relief='flat', cursor="hand2",bg="#f6f6f6")
+        self.f_pwd = Button(text="forgot password?",command=forgot_pwd,bd=0, fg="Black",underline=0 ,relief='flat', cursor="hand2",bg="#f6f6f6")
         self.f_pwd.place(x=200,y=475)
-        
+
         #Submit Button
         self.submit = Button(text="Login",command=self.verify,bd=0,bg="#7028c6",fg="#FFFFFF",relief="flat",font=('Arial', 11))
         self.submit.place(x=45,y=530,height=45,width=150)
         
-    
-        
-
     #Verifying the entered info from the user.
     def verify(self):
-        if  self.e1.get()=="" or  self.e2.get()=="":
+        if self.e1.get()=="" or self.e2.get()=="":
             self.empty()
-        elif self.e1.get()!= "Admin" or  self.e2.get()!="123":
-             self.error()
-        elif self.e1.get()== "Admin" or  self.e2.get()=="123":
+        elif self.e1.get()!= "Admin" or self.e2.get()!="123":
+            self.error()
+        elif self.e1.get()== "Admin" or self.e2.get()=="123":
             self.destroy()
         else:
             pass
@@ -77,47 +72,10 @@ class Login:
         right.config(bg="#f6f6f6",fg="#6177FE",font=('Arial',12))
         print(right)
         return
-
-def f_pwd():
-    window.destroy()
-
-    root = Tk()
-    root.title("Forgot Password")
-    root.geometry("1920x1080")
-    root.config(bg="#f6f6f6")
+        
     
-    image = Image.open('finalfpwd1.jpg')
-    image = image.resize((1600,  850), Image.ANTIALIAS)
-    my_img = ImageTk.PhotoImage(image)
-    my_lbl = Label(image = my_img) 
-    my_lbl.pack()
-     
-
-    b1 = Button( root,text="Back",bg="#f6f6f6",fg="Black",relief="flat",font=('Arial', 20))
-    b1.place(x=1440,y=40,height=50,width=80)
-
-    l1 = Label( root,text="Forgot Password",bg="#f6f6f6",fg="Black", font=('Arial', 30))
-    l1.place(x=500,y=20,height=110,width=500)
-
-    l2 = Label( root,text="Enter your Admission Number ",bg="#f6f6f6",fg="Black", font=('Arial', 11))
-    l2.place(x=320,y=250,height="40",width="200")
-
-    e1 = Entry( root,text="",bg='#EFEFEF',relief="flat")
-    e1.place(x=320,y=290,height="100",width="850")
-
-    b2 = Button( root,text="CONTINUE",bg="#7028c6",fg="#FFFFFF",relief="flat",font=('Arial', 11))
-    b2.place(x=650,y=415,height="40",width="160")
-
-
-    root.state('zoomed')
-    root.mainloop()
-
-
-
+        
 window = Tk()
 obj = Login(window)
 window.state('zoomed')
 window.mainloop()
-
-
-
--- forgot_pwd.py
+++ forgot_pwd.py
@@ -1,12 +1,48 @@
 
-from curses import window
-from tkinter import * 
+import tkinter
+from tkinter import *
+import os
+import tkinter as tk
 from PIL import Image,ImageTk
-
-from Login_page import Login
-
+#from time import sleep
 
 
+def forgot_pwd():
+
+     root = Tk()
+     root.title("Forgot Password")
+     root.geometry("1920x1080")
+     root.config(bg="#f6f6f6")
+    
+    
+     frame = Frame(root, bg='black', width=1920, height=1080)
+     image = Image.open( root,'finalpwdbg.jpg')
+     image =  image.resize((1600,  850), Image.ANTIALIAS)
+     my_img = ImageTk.PhotoImage( root,image)
+     my_lbl = Label(frame,image =  my_img) 
+     my_lbl.pack()
+
+     b1 = Button( root,text="Back",bg="#f6f6f6",fg="Black",relief="flat",font=('Arial', 20))
+     b1.place(x=1440,y=40,height=50,width=80)
+
+     l1 = Label( root,text="Forgot Password",bg="#f6f6f6",fg="Black", font=('Arial', 30))
+     l1.place(x=500,y=20,height=110,width=500)
+
+     l2 = Label( root,text="Enter your Admission Number ",bg="#f6f6f6",fg="Black", font=('Arial', 11))
+     l2.place(x=320,y=250,height="40",width="200")
+
+     e1 = Entry( root,text="",bg='#EFEFEF',relief="flat")
+     e1.place(x=320,y=290,height="100",width="850")
+
+     b2 = Button( root,text="CONTINUE",bg="#7028c6",fg="#FFFFFF",relief="flat",font=('Arial', 11))
+     b2.place(x=650,y=415,height="40",width="160")
+
+
+     root.state('zoomed')
+     root.mainloop()
+
+     
+     #sgfywuefhwefwe fweyfghwe fwehyf wheuyf hweyfweyfwgeh fwefbgwe fgbeqtffgb wefbwge 7fgbwe fbgwuefbu f nwe yfgbyewgfw
 
 
 
