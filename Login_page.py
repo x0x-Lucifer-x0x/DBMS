12dcad8298f92d7371ae83ff2d36aa11dac144ae
TheCrazyDICK <skaromal201@gmail.com>
2022-03-28 23:29:27 +0530

    Mysql Login 1.0.3

--- Login_page.py
+++ Login_page.py
@@ -103,7 +103,7 @@ def login():
     mycursor.execute(sql)
 
     if mycursor.fetchone():
-        window.destroy
+        window.destroy()
         home()
     else:
         error()
@@ -115,5 +115,6 @@ submit.place(x=45,y=530,height=45,width=150)
 
 window.state('zoomed')
 window.mainloop()
+
 
 
